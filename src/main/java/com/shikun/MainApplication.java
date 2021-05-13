d42053504c35860cb218cce9a445e10e8a72b19c
shikun <larryshi@163.com>
2021-05-13 10:00:28 +0800

    哇哇哇哇

---
+++ src/main/java/com/shikun/MainApplication.java
@@ -0,0 +1,14 @@
+package com.shikun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 告诉主程序类
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
